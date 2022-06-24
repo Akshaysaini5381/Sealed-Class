6c2e02486d8fb161f28a86fc0afbfda18a2d1186
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-25 04:26:21 +0530

    Add project files.

---
+++ Sealed Class/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sealed_Class
+{
+    sealed class test 
+    {
+    public void show()
+        {
+            Console.WriteLine("Seald class :");
+        }
+        public test()
+        {
+            Console.WriteLine("Constructor Sealed Class :");
+        }
+         static test()
+        {
+            Console.WriteLine("Constructor static  Sealed Class :");
+        }
+       
+        public static void S()
+        {
+            Console.WriteLine("Static method :");
+        }
+       
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test obj = new test();
+            obj.show();
+            test.S();
+
+        }
+    }
+}
